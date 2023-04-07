cdb79a794a6c2e6af0311efcd876010a04451777
Barrera Alan <129219461+Sartalan@users.noreply.github.com>
2023-04-06 22:50:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('#abrir').addEventListener('click',() => {
+    document.querySelector('.nav').classList.toggle('manzana')
+});
+
+document.querySelector('#cerrar').addEventListener('click', () => {
+    document.querySelector('.nav').classList.toggle('manzana') 
+});
